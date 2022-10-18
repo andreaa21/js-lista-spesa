21e8eb57771640e016d040f47772bdc7b94c1072
andreaa21 <andrea.zollini@proton.me>
2022-10-18 16:26:43 +0200

    aggiunto il br al for

--- js/script.js
+++ js/script.js
@@ -2,24 +2,24 @@
 Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo for e con un ciclo while
 */
 
-//  for
 
 const food = ['pane', 'latte', 'uova', 'pasta', 'carne', 'pesce', 'acqua'];
 const output = document.getElementById('output');
+const br = document.createElement('br');
+
+
+//  for
+
 for(let i = 0; i < food.length; i++){
     output.innerHTML += food[i];
+    output.append(br);
+    console.log(food[i]);
 }
 
 
 
 
-// while
 
-// const food = ['pane', 'latte', 'uova', 'pasta', 'carne', 'pesce', 'acqua'];
-// let c = 0;
 
-// while(c < food.lenght){
-//     console.table(food[c]);
-//     c++;
-// }
+
 
