3c968ff02d361328e6edd87ce7ac8940aad218a6
andreaa21 <andrea.zollini@proton.me>
2022-10-18 15:57:12 +0200

    soluzione for

--- js/script.js
+++ js/script.js
@@ -2,3 +2,24 @@
 Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo for e con un ciclo while
 */
 
+//  for
+
+const food = ['pane', 'latte', 'uova', 'pasta', 'carne', 'pesce', 'acqua'];
+const output = document.getElementById('output');
+for(let i = 0; i < food.length; i++){
+    output.innerHTML += food[i];
+}
+
+
+
+
+// while
+
+// const food = ['pane', 'latte', 'uova', 'pasta', 'carne', 'pesce', 'acqua'];
+// let c = 0;
+
+// while(c < food.lenght){
+//     console.table(food[c]);
+//     c++;
+// }
+
